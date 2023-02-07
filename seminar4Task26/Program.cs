1424456d65bba3da79addc188604997df3c85c42
Almaz <minnexanov.a@mail.ru>
2023-02-07 21:57:22 +0300

    seminar4Task26

---
+++ seminar4Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// задача 26. напишите программу , которая принимает
+// на вход число и выдает количество цифр в числе.
+// 456->3
+// 78->2
+// 89126->5
+
+Console.Clear();
+Console.Write("Введите число:  ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"количество цифр в числе = {NumberDigits(num)}");
+
+int NumberDigits(int number)
+{
+    int count = 1;
+    while( number > 10)
+    {
+        number = number / 10;
+        count = count + 1;
+    }
+    return count;
+}
