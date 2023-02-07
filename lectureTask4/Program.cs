0188ed5817ea0488ea71e893eb769c2f7c4cbdf0
Almaz <minnexanov.a@mail.ru>
2023-02-07 12:39:04 +0300

    lectureTask4

--- lectureTask4/Program.cs
+++ lectureTask4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while(i < count)
+    {
+        result = result + text;
+
+        i++;
+    }
+    return result; 
+}
+string res = Method4(5, "z ");
+Console.WriteLine(res);
